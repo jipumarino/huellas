8cdb6b023e1bedfb2db7c45f10328ec1b1d64ec6
Juan Ignacio Pumarino R <jipumarino@gmail.com>
2009-04-06 14:09:04 -0400

    ' -> "

--- includes/init.php
+++ includes/init.php
@@ -15,7 +15,7 @@ $filename = "$SECTIONS_DIR/$page.php";
 if(file_exists($filename)) {
   $sec_title = ucwords($page);
 } else {
-  $filename = '$SECTIONS_DIR/unknown.php';
+  $filename = "$SECTIONS_DIR/unknown.php";
   $sec_title = 'Página inexistente';
 }
   
